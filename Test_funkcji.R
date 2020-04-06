4916d2fffb474bf66427461ddc4b4553693f9eb7
MaroonBlue <martyna.majchrzak19@gmail.com>
2020-04-06 19:50:18 +0200

    defaultMethod

--- Test_funkcji.R
+++ Test_funkcji.R
@@ -8,15 +8,34 @@ summary(boys)
 summary(mtcars)
 summary(iris)
 
-## Amputacja
+## ampute
 
+### Przykład 1 - losowe usuwanie we wszystkich kolumnach
 iris_new <- iris[,-5]
 iris_amp <- ampute(data = iris_new, # dane muszą być numeryczne
                   prop = 0.5, # proporcja danych do usunięcia
-                  # patterns - ramka danych wskazująca gdzie usunąć
                   mech = "MCAR" # Missing Completely at Random 
                   )
+
 summary(iris_amp$amp)
+
+### Przykład 2 - usuwanie w 2 kolumnach w tych samych wierszach
+mtcars_amp1<-ampute(data=mtcars,
+                   # patterns - ramka danych wskazująca gdzie usunąć
+                  patterns=c(1,1,1,1,1,1,1,0,0,1,1),
+                    prop = 0.5,
+                    mech="MCAR")
+summary(mtcars_amp1$amp)
+
+### Przykład 3 - usuwanie w 2 kolumnach niezależnie (w różnych wierszach)?b
+mtcars_amp2<-ampute(data=mtcars,
+                    # patterns - ramka danych wskazująca gdzie usunąć
+                    patterns=rbind(
+                      c(1,1,1,1,1,1,1,0,1,1,1),
+                      c(1,1,1,1,1,1,1,1,0,1,1)),
+                    prop = 0.5,
+                    mech="MCAR")
+
 
 ## Wizualizacja brakujących danych
 
